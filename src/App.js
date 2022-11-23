d01659b82d4b4dc2dd28a8066984461d7224d931
edwilson.silva <edwilson.silva@rio.rj.gov.br>
2022-11-23 16:13:17 -0300

    Projeto inicial de estudos do redux-persist

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CartContainer from "./CartContainer";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <CartContainer />
+    </div>
+  );
+}
+
+export default App;
+
+
